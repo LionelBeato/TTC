45ff5ae3d4b47e66f4f87e06f4ab171eb5005bab
leo <lionel.beato@gmail.com>
2019-05-21 12:36:53 -0400

    finished lab 2 of Week 4!

--- Week_4/Lab_2/src/Lab_2.java
+++ Week_4/Lab_2/src/Lab_2.java
@@ -17,11 +17,11 @@ public class Lab_2 {
 		
 		//step two of the lab
 		
-		int z = firstInt + secondInt;
-		System.out.println(z);
+		int b = firstInt + secondInt;
+		System.out.println(b);
 		
-		int y = firstInt + thirdInt;
-		System.out.println(y);
+		int s = firstInt + thirdInt;
+		System.out.println(s);
 		
 		System.out.println((double)firstInt / secondInt);
 		System.out.println(secondInt % firstInt);
@@ -34,14 +34,115 @@ public class Lab_2 {
 		
 		int x = 100; 
 		boolean ol  = (x == 100);
+		int y = 106;
+		System.out.println(x > y);
+		System.out.println(y > x);
+		System.out.println(x >= y-6);
+
+		int z = 92; 
+		System.out.println(x < y && y < z);
+		System.out.println(y < z &&  x >= z );
+		System.out.println(y < z && x >= y);
+		System.out.println(z > (y+x) || (x-y) > z);
+		System.out.println(x < y != x > y);
 		
 		// step four of lab
+		
+		int n = 49;
+		System.out.println(n/2 == n>>1);
+		System.out.println(n * 2 ==n<<1);
+		System.out.println(n/4 == n>>2);
+
+
 		
 		
 		// step five of lab
 		
+		anotherMethod();
 		
+		//step seven in main
+		
+		playWithStrings();
 
 	}
+	
+	public static void anotherMethod()
+    {
+		
+		//step six of lab (changing ints to Integers)
+		
+		Integer r = 44;
+		Integer v = r/2;
+		String tho = "1000";
+		Integer l = Integer.parseInt(tho);
 
+		
+		if (0 > r.compareTo(l)) {
+			System.out.println(r+" is a pretty big value");
+			System.out.println(v+" is half of "+r);
+			
+			}
+			else {
+				System.out.println(r+" is not a pretty big value");
+				System.out.println(v+" is definitely small");
+			}
+			
+
+			
+		
+
+    }
+	
+	//step seven of the lab
+	
+	public static void playWithStrings()
+	{
+		String firstName = "Lionel";
+		String lastName =  "Beato";
+		String fullName = firstName.concat(" "+lastName);
+		System.out.println(fullName);
+		System.out.println(firstName+" "+lastName);
+		
+		
+		// - what is the total length of the string?
+
+		System.out.println("The full length of "+fullName+" is "+fullName.length());
+		
+	    //- index of the first 'o'
+		System.out.println("The index of the first 'o' is "+fullName.indexOf("o"));
+		
+		   // - which letter is at position 7?
+
+		System.out.println(fullName.charAt(7)+" is at position 7.");
+		
+		   // - does the full name contain the word "son"?
+
+		System.out.println(fullName.contains("son")+", the full name does not contain 'son'");
+		
+		   // - what are the last 5 letters in the full name?
+		
+		System.out.println("The last 5 letters in my full name are as follows: ");
+		
+	
+
+		System.out.println(fullName.charAt(fullName.length()-1));
+		System.out.println(fullName.charAt(fullName.length()-2));
+		System.out.println(fullName.charAt(fullName.length()-3));
+		System.out.println(fullName.charAt(fullName.length()-4));
+		System.out.println(fullName.charAt(fullName.length()-5));
+		
+		   // - does the full name contain an apostrophie or dash?
+
+		System.out.println(fullName.contains("-")+", the full name does not contain a dash! "+fullName.contains("'")+", the full name does not contain an apostrophe!");
+
+		
+
+//that's it, there's no more 
+
+
+
+		
+	}
 }
+
+
